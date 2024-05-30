e104bb7f7bafd32ca079a452bebe4d2175faac0c
Bing Wen Tan <btan@student.42singapore.sg>
2024-05-30 11:44:44 +0800

    Added checks to collect user defined functions in implementation and header files

--- internette.py
+++ internette.py
@@ -3,7 +3,15 @@
 import re
 import sys
 
-def func_finder(line):
+allowed = ['ft_isalpha', 'ft_isdigit', 'ft_isalnum', 'ft_isascii', 'ft_isprint', 'ft_strlen', 'ft_memset', 'ft_bzero', 'ft_memcpy', 'ft_memmove', 'ft_strlcpy', 'ft_strlcat', 'ft_toupper', 'ft_tolower', 'ft_strchr', 'ft_strrchr', 'ft_strncmp', 'ft_memchr', 'ft_memcmp', 'ft_strnstr', 'ft_atoi', 'ft_strdup', 'ft_calloc', 'ft_substr', 'ft_strjoin', 'ft_strtrim', 'ft_split', 'ft_itoa', 'ft_strmapi', 'ft_striteri', 'ft_putchar_fd', 'ft_putstr_fd', 'ft_putendl_fd', 'ft_putnbr_fd', 'ft_lstnew', 'ft_lstadd_front', 'ft_lstsize', 'ft_lstlast', 'ft_lstadd_back', 'ft_lstdelone', 'ft_lstclear', 'ft_lstiter', 'ft_lstmap', 'ft_printf', 'get_next_line', 'free_strs', 'ft_printf_fd', 'handle_error', 'ft_swap']
+
+def func_def(line):
+    pattern = r'\b\w+\s+[*]*?(\w+)\([^)]*\)'
+    match = re.search(pattern, line)
+    if match:
+        return match.group(1)
+
+def func_use(line):
     pattern = r'\b(\w+)\([^)]*\)'
     match = re.search(pattern, line)
     if match:
@@ -18,9 +26,17 @@ def allowed_funcs(filename):
                 allowed.append(func.split(",")[0])
     return (allowed)
 
+def permise(filename):
+    with open(filename) as file:
+        i = 1
+        allowed_func = None
+        while line := file.readline():
+            allowed_func = func_def(line.rstrip())
+            if allowed_func:
+                allowed.append(allowed_func)
+
 def interdit(filename):
-    print(f"{filename}: Error!")
-    allowed = ['ft_isalpha', 'ft_isdigit', 'ft_isalnum', 'ft_isascii', 'ft_isprint', 'ft_strlen', 'ft_memset', 'ft_bzero', 'ft_memcpy', 'ft_memmove', 'ft_strlcpy', 'ft_strlcat', 'ft_toupper', 'ft_tolower', 'ft_strchr', 'ft_strrchr', 'ft_strncmp', 'ft_memchr', 'ft_memcmp', 'ft_strnstr', 'ft_atoi', 'ft_strdup', 'ft_calloc', 'ft_substr', 'ft_strjoin', 'ft_strtrim', 'ft_split', 'ft_itoa', 'ft_strmapi', 'ft_striteri', 'ft_putchar_fd', 'ft_putstr_fd', 'ft_putendl_fd', 'ft_putnbr_fd', 'ft_lstnew', 'ft_lstadd_front', 'ft_lstsize', 'ft_lstlast', 'ft_lstadd_back', 'ft_lstdelone', 'ft_lstclear', 'ft_lstiter', 'ft_lstmap', 'ft_printf', 'get_next_line', 'free_strs', 'ft_printf_fd', 'handle_error', 'ft_swap']
+    p_flag = 0
     for each in ['open', 'close', 'read', 'write', 'malloc', 'free', 'perror', 'strerror', 'exit']:
         allowed.append(each)
     allowed.append("main")
@@ -29,13 +45,20 @@ def interdit(filename):
         i = 1
         forbidden_func = None
         while line := file.readline():
-            forbidden_func = func_finder(line.rstrip())
+            if len(forbidden) == 1 and not p_flag:
+                print(f"{filename}: Error!")
+                p_flag = 1
+            forbidden_func = func_use(line.rstrip())
             if forbidden_func and forbidden_func not in allowed:
                 forbidden.append(forbidden_func)
                 print("Error: FORBIDDEN_FUNC    ", end="")
                 print(f"(line:   {i}):  Used forbidden function:    ", end="")
                 print(forbidden_func)
             i += 1
+        if not len(forbidden):
+            print(f"{filename}: OK!")
 
+for each in sys.argv[1:]:
+    permise(each)
 for each in sys.argv[1:]:
     interdit(each)
